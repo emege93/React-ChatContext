0650a2f412048527862ce7d080f0892cf85b9d3e
emege93 <emege93@gmail.com>
2020-11-11 17:54:56 +0100

    Add Save and print msg

--- src/components/Agregar.jsx
+++ src/components/Agregar.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { ChatContext } from '../context/ChatProvider'
 
 const Agregar = () => {
+
+    const {agregarMensajes, usuario} = useContext(ChatContext)
+    const [mensaje, setMensaje] = useState('')
+
+    const agregar = (e) => {
+        e.preventDefault()
+        if(!mensaje.trim()){
+            console.log('Está vacio')
+            return
+        }
+        agregarMensajes(usuario.uid, mensaje)
+        setMensaje('')
+    }
+
+
     return (
         <form
             className="fixed-bottom input-group p-3 bg-dark"
+            onSubmit={agregar}
         >
             <input
                 type="text"
                 className="form-control"
+                value={mensaje}
+                onChange={e => setMensaje(e.target.value)}
             />
             <div className="input-group-append">
                 <button
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ChatContext } from '../context/ChatProvider'
 import Agregar from './Agregar'
 
+
 const Chat = () => {
+
+    const {mensajes, usuario} = useContext(ChatContext)
+
     return (
         <div className="mt-3 px-2">
-            <div className="d-flex justify-content-end mb-2">
-                <div className="badge badge-pill badge-primary">
-                    Mensaje del usuario activo
-                </div>
-            </div>
-            <div className="d-flex justify-content-start mb-2">
-                <div className="badge badge-pill badge-secondary">
-                    Mensaje del usuario externo
-                </div>
-            </div>
+
+            {
+                mensajes.map((item, index) => (
+                    usuario.uid === item.uid ? (
+                        <div className="d-flex justify-content-end mb-2" key={index}>
+                            <div className="badge badge-pill badge-primary">
+                                {item.texto}
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="d-flex justify-content-start mb-2" key={index}>
+                            <div className="badge badge-pill badge-secondary">
+                                {item.texto}
+                            </div>
+                        </div>
+                    )
+                ))
+            }
 
             <Agregar/>
         </div>
